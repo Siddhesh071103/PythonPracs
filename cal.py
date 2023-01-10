dc2f92279dab9db472df59b9c96740e645940317
Siddhesh071103 <103029308+Siddhesh071103@users.noreply.github.com>
2023-01-10 10:17:05 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,22 @@
+print("CALCULATOR\n")
+
+x = input("Enter the 1st number")
+y = input("Enter the 2nd number")
+
+sum = int(x) + int(y)
+sub = int(x) - int(y)
+mul = int(x) * int(y)
+div = int(x)/int(y)
+mod = int(x) % int(y)
+exp = int(x) ** int(y)
+
+print("The addition is",sum)
+print("The subtraction is",sub)
+print("The multiplication is",mul)
+print("The division is",div)
+print("The remainder is",mod)
+print("The exponent is",exp)
+
+
+
+ 
